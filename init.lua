774be3138260085071168c2075db6c89b33b4d30
franzbu <54638185+franzbu@users.noreply.github.com>
2024-09-15 18:53:29 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -448,13 +448,4 @@ function LattinMellon:handleClick()
   end
 end
 
--- helper function(s)
-function table.copy(t)
-  local t2 = {}
-  for k, v in pairs(t) do
-    t2[k] = v
-  end
-  return t2
-end
-
 return LattinMellon
