f3f1c75201a1df94af53900d885ca3ca19b9b86c
franzbu <54638185+franzbu@users.noreply.github.com>
2024-10-01 18:02:42 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -152,24 +152,19 @@ function WinHammer:new(options)
   --]]
   ---[[ -- alternative using hs.window.switcher
     -- https://applehelpwriter.com/2018/01/14/how-to-add-a-window-switcher/
-    -- set up your windowfilter
     switcher = hs.window.switcher.new() -- default windowfilter: only visible windows, all Spaces
     switcher.ui.highlightColor = {0.4,0.4,0.5,0.8}
     switcher.ui.thumbnailSize = 112
     switcher.ui.selectedThumbnailSize = 284
     switcher.ui.backgroundColor = {0.3, 0.3, 0.3, 0.5}
-    --switcher.ui.fontName = 'System'
     switcher.ui.textSize = 14
     switcher.ui.showSelectedTitle = false
-    -- bind to hotkeys; WARNING: at least one modifier key is required!
     hs.hotkey.bind("alt","tab",function()
       copy_windows_all = copyTable( windows_all)
       switcher:next()
-      -- fb: todo -> fix switching windows on current WS
     end)
     hs.hotkey.bind("alt-shift","tab",function()
       switcher:previous()
-      -- fb: todo -> fix switching windows on current WS
     end)
     --]]
 
